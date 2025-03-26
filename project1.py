5eca73cebc777879e7789071adbad3b9f173bf10
Adalarus <adalarus@gmail.com>
2025-03-26 20:12:37 +0500

    Продолэаю эксперименты.

--- project1.py
+++ project1.py
@@ -5,18 +5,34 @@ class MyWindow(QtWidgets.QWidget):
     def __init__(self, parent=None) -> None:
         QtWidgets.QWidget.__init__(self, parent)
         self.resize(800, 600)
-        self.button_exit = QtWidgets.QPushButton("&Закрыть")
+        self.widget_list = []
         self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.addWidget(self.button_exit)
         self.setLayout(self.vbox)
 
-        self.button_exit.clicked.connect(lambda: self.quit("Закрыть"))
+        self.label = QtWidgets.QLabel("______")
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.vbox.addWidget(self.label)
 
-    def quit(self, message):
+        self.add_new_widget(QtWidgets.QPushButton("1"), "Один")
+        self.add_new_widget(QtWidgets.QPushButton("2"), "Два")
+        self.add_new_widget(QtWidgets.QPushButton("3"), "Три")
+        self.add_new_widget(QtWidgets.QPushButton("Закрыть"), "Закрыть")
+
+    def on_click(self, message):
         match message:
             case "Закрыть":
                 print(message)
-                QtWidgets.QApplication.instance().quit()
+                time.sleep(1)
+                self.close()
+            case _:
+                self.label.setText(message)
+                print(message)
+
+    def add_new_widget(self, widget, message):
+        self.vbox.addWidget(widget)
+        self.widget_list.append(widget)
+        widget.clicked.connect(lambda: self.on_click(message))
+
 
 app = QtWidgets.QApplication(sys.argv)
 windows = MyWindow()
